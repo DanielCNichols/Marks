973a86197310df2246dc872dc73e57ec491f7a54
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2020-06-10 15:10:01 -0400

    updated styling and sort functionality

--- src/BookmarkControls.js
+++ src/BookmarkControls.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import './BookmarkControls.css';
 
-export default function BookmarkControls(props) {
-  const handleSort = function(ev) {
-    console.log(ev.target.sort.value);
-    ev.preventDefault();
-    props.sort(ev.target.sort.value);
+export default function BookmarkControls({ sort }) {
+  const handleSort = val => {
+    sort(val);
   };
 
   return (
     <div>
-      <form onSubmit={handleSort}>
-        <label>
-          Sort by rating
-          <select
-            className="sort-select"
-            name="sort"
-            aria-label="Sort by rating"
-          >
-            <option value="asc">Ascending</option>
-            <option value="desc">Descending</option>
-          </select>
-        </label>
-        <button className="sort">Sort</button>
-      </form>
+      <label>
+        Sort by rating
+        <select
+          className="sort-select"
+          name="sort"
+          aria-label="Sort by rating"
+          onChange={ev => handleSort(ev.target.value)}
+        >
+          <option value="">Sort by</option>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </label>
     </div>
   );
 }
