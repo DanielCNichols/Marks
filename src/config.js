1e3d3e4c0243f80455d6702885e14895d43dbad7
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2020-09-21 15:27:34 -0400

    redepoy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://mysterious-harbor-91391.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'Bearer',
   API_KEY: process.env.API_KEY,
 };
