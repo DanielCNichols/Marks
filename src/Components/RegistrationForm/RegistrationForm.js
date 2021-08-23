fdc372d819c7f5c7fa7b99faca00cc0f075c55c0
Daniel Nichols <herrnichols@gmail.com>
2021-08-23 13:31:47 -0400

    fixed routing issue with registration form. Removed console logs

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -4,26 +4,20 @@ import {
   validationRules,
 } from '../../Hooks/useRegistrationForm';
 import AuthApiService from '../../Services/authService';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import s from './RegistrationForm.module.css';
 import '../../App.css';
 
-export default function RegistrationForm(props) {
+function RegistrationForm(props) {
   const [error, setError] = useState(null);
-  const {
-    inputs,
-    handleChange,
-    handleSubmit,
-    inputErrors,
-  } = useRegistrationForm(handleRegister, validationRules);
+  const { inputs, handleChange, handleSubmit, inputErrors } =
+    useRegistrationForm(handleRegister, validationRules);
 
   async function handleRegister(inputs) {
     try {
-      console.log('running');
       await AuthApiService.postUser(inputs);
       props.history.push('/login');
     } catch (error) {
-      console.log(error);
       setError(error);
     }
   }
@@ -33,63 +27,63 @@ export default function RegistrationForm(props) {
       <fieldset>
         <legend>Sign Up</legend>
         <div className={s.formElement}>
-          <label htmlFor="username">Username</label>
+          <label htmlFor='username'>Username</label>
           {inputErrors.username && (
-            <p aria-live="assertive" className={s.error}>
+            <p aria-live='assertive' className={s.error}>
               {inputErrors.username}
             </p>
           )}
           <input
-            type="text"
-            name="username"
-            id="username"
+            type='text'
+            name='username'
+            id='username'
             onChange={handleChange}
             value={inputs.username}
-            placeholder="markit146"
-            aria-placeholder="markit146"
-            aria-required="true"
-            autoComplete="username"
+            placeholder='markit146'
+            aria-placeholder='markit146'
+            aria-required='true'
+            autoComplete='username'
           />
         </div>
         <div className={s.formElement}>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           {inputErrors.password && (
             <p className={s.error}>{inputErrors.password}</p>
           )}
           <input
-            type="password"
-            name="password"
-            id="password"
+            type='password'
+            name='password'
+            id='password'
             onChange={handleChange}
             value={inputs.password}
-            aria-required="true"
-            placeholder="Pass123"
-            aria-placeholder="Pass123"
-            autoComplete="password"
+            aria-required='true'
+            placeholder='Pass123'
+            aria-placeholder='Pass123'
+            autoComplete='password'
           />
         </div>
         <div className={s.formElement}>
-          <label htmlFor="confirmPass">Confirm Password</label>
+          <label htmlFor='confirmPass'>Confirm Password</label>
           {inputErrors.confirmPass && (
-            <p aria-live="assertive" className={s.error}>
+            <p aria-live='assertive' className={s.error}>
               {inputErrors.confirmPass}
             </p>
           )}
           <input
-            type="password"
-            name="confirmPass"
-            id="confirmPass"
+            type='password'
+            name='confirmPass'
+            id='confirmPass'
             onChange={handleChange}
             value={inputs.confirmPass}
-            aria-required="true"
-            placeholder="Pass123"
-            aria-placeholder="Pass123"
-            autoComplete="confirmPass"
+            aria-required='true'
+            placeholder='Pass123'
+            aria-placeholder='Pass123'
+            autoComplete='confirmPass'
           />
         </div>
 
         {error && (
-          <div aria-live="assertive" className={s.postError}>
+          <div aria-live='assertive' className={s.postError}>
             {Object.keys(error).map((e, idx) => {
               return (
                 <p className={s.error} key={idx}>
@@ -100,14 +94,16 @@ export default function RegistrationForm(props) {
           </div>
         )}
 
-        <button className={s.submit} type="submit">
+        <button className={s.submit} type='submit'>
           Sign up
         </button>
       </fieldset>
 
       <p className={s.loginTag}>
-        Already have an account? <Link to="/login">Log in!</Link>
+        Already have an account? <Link to='/login'>Log in!</Link>
       </p>
     </form>
   );
 }
+
+export default withRouter(RegistrationForm);
